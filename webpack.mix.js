a2cd61a7f353c99d8573c4b9fc7a5b1f5cb219e0
Timo Haapala <timo.haapala@geniem.com>
2022-03-02 13:42:33 +0200

    Font files added and compiling works

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,11 +1,17 @@
 let mix = require( 'laravel-mix' );
 
-mix.js( 'assets/scripts/theme-tredu.js', 'theme_tredu.js' )
-    .autoload( {
-        jquery: [ '$', 'window.jQuery' ],
-    } )
+// mix.webpackConfig({
+//     module: allModules
+// });
+// mix.js( 'assets/scripts/theme-tredu.js', 'theme_tredu.js' )
+//     .autoload( {
+//         jquery: [ '$', 'window.jQuery' ],
+//     } )
     // .extract() // this breaks all JS without errors
-    .sass( 'assets/styles/theme-tredu.scss', 'theme_tredu.css' )
+mix.sass( 'assets/styles/theme-tredu.scss', 'theme_tredu.css' )
+    .options( {
+        processCssUrls: false,
+    } )
     // .svgSprite(
     //     'src/icons', // The directory containing your SVG files
     //     'output/sprite.svg', // The output path for the sprite

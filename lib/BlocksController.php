9ae89d1adb0773e1b3886293da40a9972d642071
Viljami Hirvonen <viljami.hirvonen@geniem.com>
2022-04-08 10:49:48 +0300

    Add video embed

--- lib/BlocksController.php
+++ lib/BlocksController.php
@@ -191,13 +191,13 @@ class BlocksController implements Interfaces\Controller {
             //         PostType\BlogArticle::SLUG,
             //     ],
             // ],
-            // 'acf/video'          => [
-            //     'post_types' => [
-            //         PostType\Page::SLUG,
-            //         PostType\Post::SLUG,
-            //         PostType\BlogArticle::SLUG,
-            //     ],
-            // ],
+            'acf/video' => [
+                'post_types' => [
+                    PostType\Page::SLUG,
+                    PostType\Post::SLUG,
+                    PostType\BlogArticle::SLUG,
+                ],
+            ],
             // 'acf/image-gallery'  => [
             //     'post_types' => [
             //         PostType\Page::SLUG,

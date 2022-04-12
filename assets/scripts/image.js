2c539061fba072c2b2c02461619c132135eb7d39
Viljami Hirvonen <viljami.hirvonen@geniem.com>
2022-04-12 09:12:38 +0300

    Attempt to fix modaal focus trap

--- assets/scripts/image.js
+++ assets/scripts/image.js
@@ -12,6 +12,7 @@ const $ = jQuery; // eslint-disable-line no-unused-vars
  * Export the class reference.
  */
 export default class Image {
+
     /**
      * Run when the document is ready.
      *
@@ -22,48 +23,49 @@ export default class Image {
             type: 'image',
             background_scroll: false,
             after_open: this.disableBackgroundItemFocusing,
-            before_close: this.setInertToFalse,
             accessible_title: window.s.modaal.accessible_title || 'Dialog Window - Close (Press escape to close)',
             close_aria_label: window.s.modaal.close || 'Close (Press escape to close)',
         } );
     }
 
     /**
-     * A function to make all the items unfocusable except the opened lightbox gallery
+     * A function to make all the items unfocusable except the opened modaal
      */
     disableBackgroundItemFocusing() {
+        const modalContainer = document.querySelector( '.modaal-container' );
 
-        /* eslint-disable */
-        console.log(window.s);
+        // Collect each focusable element inside the modal.
+        const focusableElements = modalContainer.querySelectorAll(
+            'a[href], button:not([disabled]), [tabindex="0"]'
+        );
 
-        // Check the body if modaal-noscroll class is dynamically added, meaning that the gallery is open.
-        if ($('body').hasClass('modaal-noscroll')) {
+        // Set first and last focusable element as class parameters.
+        // Set focus to the first focusable element.
+        if ( focusableElements.length ) {
+            focusableElements.first = focusableElements[ 0 ];
+            focusableElements.last = focusableElements[ focusableElements.length - 1 ];
+            focusableElements.first.focus();
 
-            // Not cached for the .modaal-wrapper is generated dynamically, and to make sure we get every item here
-            const allContainers = $('body').children().not('.modaal-wrapper');
-
-            // Set inert to true on all the containers outside of the modaal-wrapper
-            allContainers.each((index, item) => {
-
-                // Small timeout to make sure this applies to everything - without timeout this won't work
-                window.setTimeout(item.inert = true, 100);
-
-            });
+            // Bind modal focus loop handler to document when modal is opened.
+            // If pressed key was 'Tab', call tab handling method.
+            document.addEventListener( 'keydown', ( event ) => {
+                const e = event || window.event;
+                if ( e.keyCode === 9 ) {
+                    // If shift + tab pushed.
+                    if ( e.shiftKey ) {
+                        // Focus the last element if focus was on the first element.
+                        if ( focusableElements.first === document.activeElement ) {
+                            e.preventDefault();
+                            focusableElements.last.focus();
+                        }
+                    }
+                    else if ( focusableElements.last === document.activeElement ) {
+                        e.preventDefault();
+                        focusableElements.first.focus();
+                    }
+                }
+            }
+            );
         }
-    }
-
-    /**
-     * Set inert to false on all items, used after closing the lightbox gallery - this makes the items
-     * focusable again.
-     */
-    setInertToFalse() {
-
-        // Not cached, to make sure we have everything
-        const allContainers = $('body').children().not('.modaal-wrapper');
-
-        // Set inert to true on all the containers outside of the modaal-wrapper
-        allContainers.each((index, item) => {
-            item.inert = false;
-        });
     }
 }
